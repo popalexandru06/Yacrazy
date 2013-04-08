0974b3ecbeb9f2a3b8a977fc389e9a77e2e270cc
Pop Alex <alexandrupop@reea.net>
2013-04-08 10:29:47 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yacrazy::Application.config.secret_token = '45564031fcc8f481a3835525c7b2379825d103cff570f810cc238d0e157f5e0ab6a65df3622c4d91e759c712ea233449fce0bc02c1604e9bc1c538aea843bfce'
